7946ef16b95c76de2eb143101d96e2dc9aa85370
Gajraj Singh Bhati <110357099+gsbgajraj@users.noreply.github.com>
2024-11-13 22:14:31 +0530

    Create leetcode136.single number.cpp

---
+++ leetcode136.single number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        set<int>s;
+        for(int i=0;i<nums.size();++i){
+            set.insert(nums[i]);
+        }
+        
+    }
+};
