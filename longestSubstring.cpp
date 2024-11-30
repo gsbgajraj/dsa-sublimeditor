c44a9f348cfb9367cdbbe5795b9b489109b0a22c
Gajraj singh bhati <gsbgajraj@gmail.com>
2024-11-30 21:35:59 +0530

    longest substring Blind 75 BruteForce

--- longestSubstring.cpp
+++ longestSubstring.cpp
@@ -10,10 +10,23 @@ void display(string str)
   cout<<endl;
 }
 
+int Longestsubstring(string str){
+  int maxlength=0;
+  set<char>myset;
+  for(int i=0;i<str.length();++i){
+    for(int j=i;j<str.length();++j){
+      if(myset.find(str[j])!=myset.end()) break;
+      maxlength=max(maxlength,j-i+1);
+      myset.insert(str[j]);
+    }
+  }
+  return maxlength;
+}
+
 int main(){
     string str;
     cin>>str;
     display(str);
+    cout<<Longestsubstring(str);
     return 0;
-
 }
