7ec13b92b16d156216bbbc6a51733cdcee7656d5
Gajraj singh bhati <gsbgajraj@gmail.com>
2024-11-30 21:26:01 +0530

    printing strings

---
+++ longestSubstring.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void display(string str)
+{
+  for(int i=0;i<str.length(); i++)
+  {
+    cout<<str[i];
+  }
+  cout<<endl;
+}
+
+int main(){
+    string str;
+    cin>>str;
+    display(str);
+    return 0;
+
+}
